b1aac6f5c4de20de3f4e51b1f195ae5978e8b637
alfiyya <97938553+alfiyya@users.noreply.github.com>
2022-02-10 14:17:57 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,26 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
- int k;
 
-struct{
+struct siswa{
  char nama[20];
  int nim[10];
  int kehadiran[20];
- }dt[5];
+ };
 
-printf("=====Masukan Data Mahasiswa \n\n");
+int main (void)
+{
+    int i;
+    struct siswa kls[5];
+    printf("=====Masukan Data Mahasiswa \n\n");
 
-for(k=0;k<5;k++)
- {
- printf("No. %i \n",k+1);
- printf("Nama = "); scanf("%s",&dt[k].nama);
- printf("Nim = "); scanf("%i",&dt[k].nim);
- printf("Kehadiran="); scanf("%i",&dt[k].kehadiran);
- 
- if ((dt[k].kehadiran)<80){
-    printf("%d. %s//%d//%.0f\n", k+1,dt[k].nama,dt[k].nim,dt[k].kehadiran);}}
- return(0);
- }
+    for(i=0;i<5;i++)
+    {
+        printf("No. %i \n",i+1);
+        printf("Nama = "); scanf("%s",&kls[i].nama);
+        printf("Nim = "); scanf("%i",&kls[i].nim);
+        printf("Kehadiran="); scanf("%i",&kls[i].kehadiran);
+    }
+    printf("Mahasiswa yang kurang dari 80 persen adalah ");
+
+    for(i=0;i<5;i++)
+    {
+        if ((kls[i].kehadiran)<80){
+            printf("%d. %s//%d//%.0f\n", i+1,kls[i].nama,kls[i].nim,kls[i].kehadiran);}
+    }
+
+}
+
+
